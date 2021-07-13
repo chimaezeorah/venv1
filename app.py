5bec2aaf967dde8481ff9a45f47edcf7984efd4a
getfit BIE <chima@getfit.ng>
2021-07-13 14:58:42 +0100

    new change v2

--- app.py
+++ app.py
@@ -1,101 +1,104 @@
-import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
 import pandas as pd
-import time
 
-import mysql.connector as mysql
+data = pd.read_csv("avocado.csv")
+data = data.query("type == 'conventional' and region == 'Albany'")
+data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
 
-# enter your server IP address/domain name
-while True:
-    time.sleep(5)
-
-    # enter your server IP address/domain name
-    HOST = "sql5.freemysqlhosting.net"  # or "domain.com"
-    # database name, if you want just to connect to MySQL server, leave it empty
-    DATABASE = "sql5424622"
-    # this is the user you create
-    USER = "sql5424622"
-    # user password
-    PASSWORD = "wMwKZ98X7u"
-    # connect to MySQL server
-    db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
-    print("Connected to:", db_connection.get_server_info())
-
-    # while True:
-    # sleep(60 - time() % 60)
-
-    cursor = db_connection.cursor()
-    # get database information
-    cursor.execute("select database();")
-    database_name = cursor.fetchone()
-    print("[+] You are connected to the database:", database_name)
-    # fetch the database
-    df = cursor.execute("select * from tblfood")
-    # get all selected rows
-    rows = cursor.fetchall()
-    columns = ["ID", "CategoryName", "ItemName", "ItemPrice", "ItemDes", "Image", "ItemQty"]
-    df = pd.DataFrame(rows, columns=columns)
-
-    print("all imported data")
-    print("looping again")
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+        "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Avocado Analytics: Understand Your Avocados!"
 
-server = app.server
-
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Mangoes", "Oranges", "Bananas", "Mangoes", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
+app.layout = html.Div(
+    children=[
+        html.Div(
+            children=[
+                html.P(children="🥑", className="header-emoji"),
+                html.H1(
+                    children="Avocado Analytics", className="header-title"
+                ),
+                html.P(
+                    children="Analyze the behavior of avocado prices"
+                    " and the number of avocados sold in the US"
+                    " between 2015 and 2018",
+                    className="header-description",
+                ),
+            ],
+            className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="price-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["Date"],
+                                    "y": data["AveragePrice"],
+                                    "type": "lines",
+                                    "hovertemplate": "$%{y:.2f}"
+                                    "<extra></extra>",
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Average Price of Avocados",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                                "xaxis": {"fixedrange": True},
+                                "yaxis": {
+                                    "tickprefix": "$",
+                                    "fixedrange": True,
+                                },
+                                "colorway": ["#17B897"],
+                            },
+                        },
+                    ),
+                    className="card",
+                ),
+                html.Div(
+                    children=dcc.Graph(
+                        id="volume-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                                {
+                                    "x": data["Date"],
+                                    "y": data["Total Volume"],
+                                    "type": "lines",
+                                },
+                            ],
+                            "layout": {
+                                "title": {
+                                    "text": "Avocados Sold",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                                "xaxis": {"fixedrange": True},
+                                "yaxis": {"fixedrange": True},
+                                "colorway": ["#E12D39"],
+                            },
+                        },
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
+        ),
+    ]
 )
 
-
-
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig
-    )
-])
-
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
