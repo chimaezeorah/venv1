1aa2df086e72f5d16330ddbe0c17d973912a4508
getfit BIE <chima@getfit.ng>
2021-07-13 12:38:31 +0100

    new change v2

--- app.py
+++ app.py
@@ -6,6 +6,42 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
+import mysql.connector as mysql
+
+# enter your server IP address/domain name
+while True:
+    time.sleep(5)
+
+    # enter your server IP address/domain name
+    HOST = "sql5.freemysqlhosting.net"  # or "domain.com"
+    # database name, if you want just to connect to MySQL server, leave it empty
+    DATABASE = "sql5424622"
+    # this is the user you create
+    USER = "sql5424622"
+    # user password
+    PASSWORD = "wMwKZ98X7u"
+    # connect to MySQL server
+    db_connection = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+    print("Connected to:", db_connection.get_server_info())
+
+    # while True:
+    # sleep(60 - time() % 60)
+
+    cursor = db_connection.cursor()
+    # get database information
+    cursor.execute("select database();")
+    database_name = cursor.fetchone()
+    print("[+] You are connected to the database:", database_name)
+    # fetch the database
+    df = cursor.execute("select * from tblfood")
+    # get all selected rows
+    rows = cursor.fetchall()
+    columns = ["ID", "CategoryName", "ItemName", "ItemPrice", "ItemDes", "Image", "ItemQty"]
+    df = pd.DataFrame(rows, columns=columns)
+
+    print("all imported data")
+    print("looping again")
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
